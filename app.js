7fbc1120f484822552656c1e5d99781b7e279c21
Oleg <hellreebok@gmail.com>
2022-11-28 16:48:59 +0200

    update vercel.json

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
+const mongoose = require("mongoose"); ///////////
+const { DB_HOST, PORT = 4000 } = process.env; //////////////
+
 const usersRouter = require("./routes/api/users.js");
 const productsRouter = require("./routes/api/products.js");
 
@@ -29,5 +32,18 @@ app.use((err, req, res, next) => {
     const { status = 500, message = "Server error" } = err;
     res.status(status).json({ message });
 });
+
+mongoose
+    .connect(DB_HOST)
+    .then(() => {
+        console.log("Database connection successful");
+        app.listen(PORT, () => {
+            console.log(`Server running. Use our API on port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error.message);
+        process.exit(1);
+    }); ///////////////////////
 
 module.exports = app;
